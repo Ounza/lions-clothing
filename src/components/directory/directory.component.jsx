9f725c6dd4a32e328d174a6ac751eadc62f36e5f
Rush <rmounza@gmail.com>
2021-05-14 14:05:51 +0300

    Created the project with create-react-app and added the homepage component with the menu items

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss';
+class Directory extends Component {
+    state = {  
+       sections: [
+        {
+            title: 'hats',
+            imageUrl: '../../images/hats_image.jpg',
+            id: 1,
+            linkUrl: 'shop/hats'
+          },
+          {
+            title: 'jackets',
+            imageUrl: '../../images/jackets_image.jpg',
+            id: 2,
+            linkUrl: 'shop/jackets'
+          },
+          {
+            title: 'sneakers',
+            imageUrl: '../../images/sneakers_image.jpg',
+            id: 3,
+            linkUrl: 'shop/sneakers'
+          },
+          {
+            title: 'womens',
+            imageUrl: '../../images/woman_image.jpg',
+            size: 'large',
+            id: 4,
+            linkUrl: 'shop/womens'
+          },
+          {
+            title: 'mens',
+            imageUrl: '../../images/man_image.jpg',
+            size: 'large',
+            id: 5,
+            linkUrl: 'shop/mens'
+          }
+       ] 
+    }
+    render() { 
+        return ( 
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+         );
+    }
+}
+ 
+export default Directory;
